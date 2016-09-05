283fa5501deb9723fc9b046151b225c5f56f5f3e
Ankhbayar <b_ankhbayar@outlook.com>
2016-09-05 12:42:57 +0900

    Revert "Printing Goodbye World!"
    This reverts commit 83b146870c0d79f511957c65ae42a610c094b7c6.

--- GitTest01/Program.cs
+++ GitTest01/Program.cs
@@ -14,7 +14,7 @@ namespace GitTest01
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Goodbye World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
